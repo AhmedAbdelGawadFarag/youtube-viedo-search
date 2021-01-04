7f72fcf622905cab2e53d02e467c391552dc6ecf
ahmed beeka <ahmed.abdelgawad.farag@gmail.com>
2021-01-04 14:39:15 +0200

    init commit

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+class App extends React.Component{
+
+    render(){
+        return(
+            <div>
+                init react commit
+            </div>
+        );
+    }
+
+
+}
+export default App;
