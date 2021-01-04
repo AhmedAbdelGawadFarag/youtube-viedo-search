4d145c523113904d551179eba72807b1aeffe1b0
ahmed beeka <ahmed.abdelgawad.farag@gmail.com>
2021-01-04 22:49:52 +0200

    added google api

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,14 @@
 import React from "react";
 import SearchBar from "./SearchBar";
+import GoogleApi from '../api/GoogleApi';
 class App extends React.Component {
   callApi(term) {
     console.log(term);
+    GoogleApi.get("/search",{
+      params:{
+        q:term
+      }
+    });
   }
 
   render() {
